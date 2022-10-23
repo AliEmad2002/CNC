ca32e84e138d87a035d920b6d821c4c26cd47cbc
AliEmad2002 <99054912+AliEmad2002@users.noreply.github.com>
2022-10-23 23:20:59 +0200

    Add files via upload

---
+++ MCU/Inc/CNC/CNC_private.h
@@ -0,0 +1,13 @@
+/*
+ * CNC_private.h
+ *
+ *  Created on: Sep 19, 2022
+ *      Author: Ali Emad
+ */
+
+#ifndef _CNC_PRIVATE_H_
+#define _CNC_PRIVATE_H_
+
+#define WRONG_INTERPOLATION_POINT_ERR_CODE		0
+
+#endif /* _CNC_PRIVATE_H_ */
