043e9eafe125799f7741d19ea24528132696ead8
AliEmad2002 <ali.20352473@eng.aun.edu.eg>
2023-08-01 02:22:09 +0300

    Update CNC_program.c

--- MCU/Src/APP/CNC_program.c
+++ MCU/Src/APP/CNC_program.c
@@ -1024,6 +1024,9 @@ void CNC_voidExecuteAutoLevelingSampling(CNC_t* CNC)
 	if (CNC->config.autoLevelingEnabled)
 		return;
 
+	/*	Enable auto leveling	*/
+	CNC->config.autoLevelingEnabled = 1;
+
 #if !SIMULATION_ON
 	/**
 	 * initially probe on (x, y) = (0, 0), and re-set z-displacement variable.
@@ -1059,8 +1062,6 @@ void CNC_voidExecuteAutoLevelingSampling(CNC_t* CNC)
 	}
 
 	/**	init params	**/
-	CNC->config.autoLevelingEnabled = 1;
-
 	/*	N = V + 3	(G-Code convention)	*/
 	CNC->map.nX = (u8)CNC->point[4] + 3;
 	CNC->map.nY = (u8)CNC->point[5] + 3;
